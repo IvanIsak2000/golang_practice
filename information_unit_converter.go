bc11536127f3a6fbc7c4b86223ae21bbd6d73b58
IvanIsak2000 <vingving648@gmail.com>
2023-10-07 19:48:21 +0300

    rename names, func, small code upgrate

--- information_unit_converter.go
+++ information_unit_converter.go
@@ -5,23 +5,23 @@ import (
 	"math"
 )
 
-func get_number() float64 {
+func getNumber() float64 {
 	var number float64
 	fmt.Print("Please enter a number: ")
 	fmt.Scan(&number)
 	return number
 }
 
-func get_unit_of_information() int {
-	var number_unit int
+func getUnitOfInformation() int {
+	var numberUnit int
 	fmt.Println("\n(1)Bit\n(2)Byte\n(3)Kbyte\n(4)Mbyte\n(5)Gbyte\n(6)Tbyte")
-	fmt.Scan(&number_unit)
-	return number_unit
+	fmt.Scan(&numberUnit)
+	return numberUnit
 }
 
-func calculation(number float64, primary_unit int, finish_unit int) float64 {
-	p := float64(primary_unit)
-	f := float64(finish_unit)
+func calculation(number float64, primaryUnit int, finishUnit int) float64 {
+	p := float64(primaryUnit)
+	f := float64(finishUnit)
 	if f != p {
 		if p == 1 || p == 2 && f == 1 || f == 2 {
 			if p > f {
@@ -31,7 +31,7 @@ func calculation(number float64, primary_unit int, finish_unit int) float64 {
 		}
 
 		if p > f {
-			diff := float64(p - f)
+			diff := p - f
 			return number * math.Pow(1024, diff)
 			//switch diff {
 			//case 1:
@@ -51,16 +51,16 @@ func calculation(number float64, primary_unit int, finish_unit int) float64 {
 
 func main() {
 	units := [...]string{1: "Bit", 2: "Byte", 3: "Kbyte", 4: "Mbyte", 5: "Gbyte", 6: "Tbyte"}
-	number := get_number()
+	number := getNumber()
 
 	fmt.Print("Please write a primary the  unit of information:")
-	primary_unit := get_unit_of_information()
+	primaryUnit := getUnitOfInformation()
 
 	fmt.Print("Please write the finish unit of information:")
-	finish_unit := get_unit_of_information()
+	finishUnit := getUnitOfInformation()
 
-	answer := calculation(number, primary_unit, finish_unit)
+	answer := calculation(number, primaryUnit, finishUnit)
 	if answer != -1 {
-		fmt.Printf("Answer: %f %s = %f %s", number, units[primary_unit], answer, units[finish_unit])
+		fmt.Printf("Answer: %f %s = %f %s", number, units[primaryUnit], answer, units[finishUnit])
 	}
 }
